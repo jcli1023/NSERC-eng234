78ff5826efdcfabf391f5a7f10739879388967bc
jcli1023 <jcli@scs.ryerson.ca>
2016-09-12 16:45:55 -0400

    added camera folder and date to names of frames

--- saveScreenshot.php
+++ saveScreenshot.php
@@ -2,16 +2,16 @@
 	//echo "I entered PHP<br>";
 	//data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDABALDA4
 	$data = $_POST['frame'];
-	echo $data;
+	$camNum = $_POST['cam'];
+	$time = $_POST['time'];
+	$camFolder = "CAMERA ".$camNum;
 	list($type, $data) = explode(';', $data);
 	list(, $data)      = explode(',', $data);
 	$data = base64_decode($data);
 
-	if (!file_exists("./photos"))	
-		mkdir("./photos");
-	file_put_contents( "./photos/".uniqid().'.jpg', $data);
+	if (!file_exists("./".$camFolder))	
+		mkdir("./".$camFolder);
+	file_put_contents( "./".$camFolder."/".$time.'.jpg', $data);
 	//file_put_contents( "./photos/".uniqid().'.png', $data);
-	//echo "I fini PHP\n";
-	
-	die("Went through PHP");
+	die();
 ?>
