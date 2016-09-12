210b401bb71fa2b013f70b5a5aeddf1a9bb8c2fd
jcli1023 <jcli@scs.ryerson.ca>
2016-09-12 16:53:25 -0400

    added frame captures to camera2

--- saveScreenshot.php
+++ saveScreenshot.php
@@ -1,10 +1,8 @@
 <?php
-	//echo "I entered PHP<br>";
-	//data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDABALDA4
 	$data = $_POST['frame'];
 	$camNum = $_POST['cam'];
 	$time = $_POST['time'];
-	$camFolder = "CAMERA ".$camNum;
+	$camFolder = "CAMERA-".$camNum;
 	list($type, $data) = explode(';', $data);
 	list(, $data)      = explode(',', $data);
 	$data = base64_decode($data);
@@ -12,6 +10,5 @@
 	if (!file_exists("./".$camFolder))	
 		mkdir("./".$camFolder);
 	file_put_contents( "./".$camFolder."/".$time.'.jpg', $data);
-	//file_put_contents( "./photos/".uniqid().'.png', $data);
 	die();
 ?>
