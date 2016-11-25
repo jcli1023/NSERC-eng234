6dc877b954e1a4c6be0f9c0c47352caa1f450883
jcli1023 <jcli@ryerson.ca>
2016-11-24 20:16:17 -0500

    Added emailing if object is missing

--- email.php
+++ email.php
@@ -1,9 +1,9 @@
 <?php
-$to = $POST['email'];
-$time = $POST['reportTimeFrame'];
+$to = $_POST['email'];
+$time = $_POST['time'];
 $subject = "Object Missing ". $time;
 $txt = "Tracked Object was missing at ". $time;
-$headers = "From: webmaster@dsmp.ryerson.ca" . "\r\n" .
+$headers = "From: webmaster@dsmp.ryerson.ca" . "\r\n";
 mail($to,$subject,$txt,$headers);
 echo "email sent to " . $to;
 ?>
