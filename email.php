9070ed79dd0e01fed448f5d16b8686f6830e7cca
jcli1023 <jcli@ryerson.ca>
2016-12-01 14:40:23 -0800

    Added second email, changed default second video

--- email.php
+++ email.php
@@ -1,8 +1,9 @@
 <?php
 $to = $_POST['email'];
+$camNum = $_POST['camNum'];
 $time = $_POST['time'];
-$subject = "Object Missing ". $time;
-$txt = "Tracked Object was missing at ". $time;
+$subject = "Camera-".$camNum." Object Missing ". $time;
+$txt = "Camera-".$camNum." Tracked Object was missing at ". $time;
 $headers = "From: webmaster@dsmp.ryerson.ca" . "\r\n";
 mail($to,$subject,$txt,$headers);
 echo "Email sent to " . $to;
