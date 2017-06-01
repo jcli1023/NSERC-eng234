d4da0de5580ee7ae8fe53b9ed4e0edcd8de9e558
jcli1023 <jcli@ryerson.ca>
2017-06-01 13:55:58 -0700

    fixed space error in compile for kmeans.php

--- kmeans.php
+++ kmeans.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-$resultsCompile = shell_exec("javac -cp weka.jar:ClusterWeka.java");
+$resultsCompile = shell_exec("javac -cp weka.jar: ClusterWeka.java");
 $results = shell_exec("java -cp weka.jar: ClusterWeka 2>&1");
 //$resultsTest = shell_exec("ls 2>&1");
 
